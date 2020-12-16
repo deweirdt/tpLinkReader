b4f8147f7f799840863015b45d902bf66786bb51
Deweirdt Peter-Jan <deweirdt@gmail.com>
2020-12-16 13:36:03 +0100

    fixing the date

--- tp-link-reader.js
+++ tp-link-reader.js
@@ -9,16 +9,19 @@ const client = new Client();
 // Client events `device-*` also have `bulb-*` and `plug-*` counterparts.
 // Use those if you want only events for those types and not all devices.
 client.on('device-new', (device) => {
-  device.startPolling(5000);
+  device.startPolling(1000);
   
   // Device (Common) Events
   device.on('emeter-realtime-update', (emeterRealtime) => {
 
     let plug = new Plug(device);
     let dayStats = 0;
-    plug.emeter.getDayStats(2020, 11).then(data => {
-        let date_ob = new Date().getDate() - 1;
-        console.log("day_list is: ", data.day_list);
+
+    let date = new Date();
+    plug.emeter.getDayStats(2020, date.getMonth() +1 ).then(data => {
+        let date_ob = date.getDate() - 1;
+        console.log("Data received: %s", date);
+        console.log("day_list is: %s: ", date, data.day_list);
         dayStats = data.day_list[date_ob].energy_wh;
         
         let iotData = {
@@ -35,7 +38,7 @@ client.on('device-new', (device) => {
             }
         } 
         amqp.publishAMQP(iotData);
-        console.log("iotData: ", iotData);
+        console.log("iotData: %s: ", date, iotData);
     });
   });
 });
